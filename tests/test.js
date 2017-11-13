954d2968e15a6f816fff24c4e4086dd60a2eb635
busterbunny69 <buster.bunny.69@gmail.com>
2017-11-13 14:29:14 +0100

    Update test.js

--- tests/test.js
+++ tests/test.js
@@ -11,6 +11,6 @@ describe('# 42 is 42', () => {
 describe('# 0 is 0', () => {
   it('should equal 0', () => {
     console.log("peace and prosperity")
-    expect(0).to.equal(42);
+    expect(0).to.equal(0);
   });
 });
