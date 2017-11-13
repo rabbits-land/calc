c126a969924df755ce53252997ac9d9d3ef504da
Philippe Charrière <ph.charriere@gmail.com>
2017-11-13 14:03:28 +0100

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('# 42 is 42', () => {
+  it('should equal 42', () => {
+    expect(42).to.equal(42);
+  });
+});
+
+describe('# 0 is 0', () => {
+  it('should equal 0', () => {
+    console.log("peace and prosperity")
+    expect(0).to.equal(0);
+  });
+});
